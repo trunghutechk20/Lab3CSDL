db4065f0dd099ad3fdbaf4d46958172901a520f6
Your Name <trunghutechk20@gmail.com>
2023-05-25 11:06:21 +0700

    tuan4

--- src/main/java/com/example/BuiQuangTrung/controller/BookController.java
+++ src/main/java/com/example/BuiQuangTrung/controller/BookController.java
@@ -3,10 +3,16 @@ package com.example.BuiQuangTrung.controller;
 
 import com.example.BuiQuangTrung.entity.Book;
 import com.example.BuiQuangTrung.services.BookService;
+import com.example.BuiQuangTrung.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,10 +21,27 @@ import java.util.List;
 public class BookController {
     @Autowired
     private BookService bookService;
+    @Autowired
+    private CategoryService categoryService;
     @GetMapping
     public String showAllBooks (Model model){
         List<Book> books = bookService.getAllBooks();
         model.addAttribute("books",books);
         return "book/list";
+    }
+    @GetMapping ("/add")
+    public String addBookForm (Model model){
+        model.addAttribute("book", new Book());
+        model.addAttribute("categories",categoryService.getAllCategories());
+        return "book/add";
+    }
+    @PostMapping("/add")
+    public String addBook(@Validated @ModelAttribute("book") Book book, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/add";
+        }
+        bookService.addBook(book);
+        return "redirect:/books";
     }
 }
